33ee2a88483d44af05cc1e04c99ff1d0b33ba81f
Casta-mere <castamerego@gmail.com>
2024-09-02 21:19:10 +0800

    system-modify: modify home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,42 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import React from 'react';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Typewriter from "@site/src/components/TypeWriter";
+import Layout from "@theme/Layout";
+import React from "react";
 
-const taglines = [
+const waitingForLove = [
   "Monday left me broken",
   "Tuesday, I was through with hoping",
   "Wednesday, my empty arms are open",
   "Thursday, waiting for love, waiting for love",
   "Thank the stars, it's Friday",
   "I'm burning like a fire gone wild on Saturday",
-  "Guess I won't be coming to church on Sunday"
-]
+  "Guess I won't be coming to church on Sunday",
+];
+
+const intro = ["全栈开发", "EDG! 我们是冠军!"];
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  
+  const { siteConfig } = useDocusaurusContext();
   const weekday = new Date().getDay();
+
+  const lines =
+    waitingForLove[weekday - 1] + ";" + intro.map((line) => line).join(";");
   return (
     <header>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{taglines[weekday - 1]}</p>
+        <h1 className="hero__title  padding-top--lg">{siteConfig.title}</h1>
+        <p className="hero__subtitle">
+          <Typewriter text={lines} delinterval={5000} />
+        </p>
       </div>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      description="Volar Morgulis">
+    <Layout description="Volar Morgulis">
       <HomepageHeader />
     </Layout>
   );
