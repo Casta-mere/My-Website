f655d544bbb12df31908e9c0996a7878fb103660
Casta-mere <castamerego@gmail.com>
2024-09-06 11:32:38 +0800

    blog-modify: 模拟命令行终端小组件 V1.2 Add mysql mode

--- blog/2024/09/03-cmdTerminal/components/Terminal.tsx
+++ blog/2024/09/03-cmdTerminal/components/Terminal.tsx
@@ -1,7 +1,10 @@
 import {
   Args,
   Cmd,
+  Dbkey,
+  Emph,
   Line,
+  MysqlLine,
   TerminalLine,
   TerminalResponse,
   TerminalRoot,
@@ -27,15 +30,14 @@ export default function Terminal({}) {
       >
         <Line text="PID TTY          TIME CMD" />
         <Line text=" 12 tty1     00:00:00 sh" />
-        <Line text=" 13 tty1     00:00:00 sh" />
-        <Line text=" 18 tty1     00:00:00 sh" />
-        <Line text=" 22 tty1     00:00:08 node" />
+        <Emph text=" 22 tty1     00:00:08 node" />
       </TerminalResponse>
 
       <TerminalLine userName="casta" dir="" conda="base">
         <Cmd text="conda activate" />
         <Line text="torch" />
       </TerminalLine>
+
       <TerminalLine userName="casta" dir="" conda="torch">
         <Cmd text="ll" />
       </TerminalLine>
@@ -47,6 +49,37 @@ export default function Terminal({}) {
       >
         <Line text="total 1" />
         <Line text="drwxr-xr-x 2 root root 4096 Sep  3 10:00 ./" />
+      </TerminalResponse>
+
+      <TerminalLine userName="casta" dir="" conda="torch">
+        <Cmd text="mysql" />
+        <Args text="-uroot -p" />
+      </TerminalLine>
+
+      <MysqlLine linebreak="MANUAL">
+        <Dbkey text="use" />
+        <Line text="my_db;" />
+        <br />
+        <Dbkey text="select" />
+        <Line text="*" />
+        <br />
+        <Dbkey text="from" />
+        <Line text="user" />
+        <br />
+        <Dbkey text="orderby" />
+        <Line text="id" />
+        <Dbkey text="desc" />
+        <Line text=";" />
+      </MysqlLine>
+
+      <TerminalResponse response_style="NEWLINE">
+        <Line text="+--------+-------------+-----------------------+" />
+        <Line text="| id     | name        | email                 |" />
+        <Line text="+--------+-------------+-----------------------+" />
+        <Line text="| cltaau | Castamere   | castamerego@gmail.com |" />
+        <Line text="| cltx76 | Achinoise   | 981193371@qq.com      |" />
+        <Line text="| cltzwk | flamingo952 | 1719538377@qq.com     |" />
+        <Line text="+--------+-------------+-----------------------+" />
       </TerminalResponse>
     </TerminalRoot>
   );
