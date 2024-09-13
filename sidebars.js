6ce4d7ebb3ba1474240c06268da26fe1d2913070
Casta-mere <castamerego@gmail.com>
2024-09-13 13:58:39 +0800

    system-modify: doc/Latex pages modify

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,24 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  tutorialSidebar: ["Intro", "Rules", "Resume"],
+  tutorialSidebar: [
+    "Intro",
+    {
+      type: "category",
+      label: "Latex",
+      link: {
+        type: "doc",
+        id: "Latex/README",
+      },
+      items: [
+        "Latex/Latex",
+        "Latex/Reference",
+        "Latex/Numbering",
+        "Latex/Style",
+      ],
+    },
+    "Rules",
+    "Resume",
+  ],
 };
 
 module.exports = sidebars;
