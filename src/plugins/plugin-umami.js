8b7be8210292bf6eb0f3a3ea5a73fd373765d707
Casta-mere <castamerego@gmail.com>
2024-09-27 17:24:05 +0800

    system-modify: Set https for umami

--- src/plugins/plugin-umami.js
+++ src/plugins/plugin-umami.js
@@ -9,14 +9,14 @@ async function umamiPlugin() {
             tagName: "link",
             attributes: {
               rel: "preconnect",
-              href: "http://umami.castamerego.com/",
+              href: "https://umami.castamerego.com/",
             },
           },
           {
             tagName: "script",
             attributes: {
               defer: true,
-              src: "http://umami.castamerego.com//script.js",
+              src: "https://umami.castamerego.com//script.js",
               "data-website-id": "e993a914-0864-4b88-a53c-75e46dc174d7",
             },
           },
