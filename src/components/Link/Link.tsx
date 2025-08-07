6479980bc04d370cb358e46290759815d50a8eba
Casta-mere <castamerego@gmail.com>
2025-08-07 17:11:50 +0800

    system-modify: upgrade Link component to V0.2

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,36 +1,64 @@
-import { useColorMode } from "@docusaurus/theme-common";
+import { useLocation } from "@docusaurus/router";
+import { useColorMode as useDocusaurusColorMode } from "@docusaurus/theme-common";
 import classNames from "classnames";
 import React, { useRef, useState } from "react";
 
 interface Props {
   title: string;
   url: string;
+  size?: "sm" | "lg";
+  open?: "newTab" | "sameTab";
+  onSameUrl?: "open" | "refresh" | "disable";
+  colorMode?: "dark" | "light" | "custom";
 }
 
 const colors = [
   {
     main: "text-blue-500",
-    hover: "hover:text-blue-600",
-    underScore: "bg-blue-400",
+    hover: "hover:text-orange-500",
+    underScore: "bg-orange-500",
   },
   {
-    main: "text-gray-500",
-    hover: "hover:text-gray-400",
-    underScore: "bg-gray-300",
+    main: "text-blue-400",
+    hover: "hover:text-violet-500",
+    underScore: "bg-violet-500",
+  },
+  {
+    main: "text-gray-300",
+    hover: "hover:text-violet-500",
+    underScore: "bg-violet-500",
   },
 ];
 
-const Link = ({ title, url }: Props) => {
+const Link = ({
+  title,
+  url,
+  open = "newTab",
+  onSameUrl = "refresh",
+  size = "sm",
+  colorMode,
+}: Props) => {
   const [hover, setHover] = useState(false);
   const [leaving, setLeaving] = useState(true);
   const timeoutId = useRef<number | null>(null);
-  const color = colors[useColorMode().colorMode === "dark" ? 1 : 0];
+  let mode: "dark" | "light" | "custom";
+  if (colorMode) {
+    mode = colorMode;
+  } else {
+    try {
+      mode = useDocusaurusColorMode().colorMode;
+    } catch {
+      mode = "light";
+    }
+  }
+  const color = colors[mode === "dark" ? 1 : mode === "light" ? 0 : 2];
+  const isSameUrl = useLocation().pathname === url;
 
   return (
     <a
-      href={url}
+      href={isSameUrl ? (onSameUrl === "disable" ? undefined : url) : url}
       className={classNames({
-        "relative inline-block transition-all duration-300 ": true,
+        "relative inline-block": true,
         [color.main]: true,
         [color.hover]: true,
       })}
@@ -40,24 +68,43 @@ const Link = ({ title, url }: Props) => {
           clearTimeout(timeoutId.current);
         }
         timeoutId.current = window.setTimeout(() => {
+          timeoutId.current = null;
           setLeaving(false);
         }, 500);
       }}
       onMouseLeave={() => {
         if (timeoutId.current) {
           clearTimeout(timeoutId.current);
+          timeoutId.current = window.setTimeout(() => {
+            setHover(false);
+            setLeaving(false);
+          }, 300);
+          window.setTimeout(() => {
+            setLeaving(true);
+          }, 350);
+        } else {
+          setHover(false);
+          setLeaving(true);
         }
-        setHover(false);
-        setLeaving(true);
       }}
-      target="_blank"
+      target={
+        open === "newTab"
+          ? isSameUrl
+            ? onSameUrl === "open"
+              ? "_blank"
+              : undefined
+            : "_blank"
+          : undefined
+      }
       rel="noopener noreferrer"
     >
       {title}
 
       <span
         className={classNames({
-          "absolute right-0 bottom-0 h-0.5 ": true,
+          "absolute right-0 h-0.5 ": true,
+          "bottom-0": size === "sm",
+          "-bottom-1": size === "lg",
           [color.underScore]: true,
           "w-0 transition-all duration-500": leaving,
           "w-full": !leaving,
@@ -65,7 +112,9 @@ const Link = ({ title, url }: Props) => {
       />
       <span
         className={classNames({
-          "absolute left-0 bottom-0 h-0.5": true,
+          "absolute left-0 h-0.5": true,
+          "bottom-0": size === "sm",
+          "-bottom-1": size === "lg",
           [color.underScore]: true,
           "w-full transition-all duration-500": hover,
           "w-0": !hover,
---
+++ src/components/Link/README.md
@@ -0,0 +1,10 @@
+# Link
+
+React 超链接小组件
+
+[这里](https://castamerego.com/docs/Snippets/Components/Link) 介绍了这个小组件
+
+## 版本记录
+
+- **V0.1** 基础样式实现
+- **V0.2** 优化细节，参考[超链接小组件](https://castamerego.com/blog/Link)
