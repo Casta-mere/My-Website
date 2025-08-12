67e6c8d5843846c288c8007e265e8911bf78ba5d
Casta-mere <castamerego@gmail.com>
2025-08-12 15:42:03 +0800

    blog-modify: Link-update-2025-08 V0.1 悬浮窗介绍完成

---
+++ blog/2025/08/11-Link-update-2025-08/components/Link3.tsx
@@ -0,0 +1,87 @@
+import classNames from "classnames";
+import React from "react";
+
+const Link3 = () => {
+  return (
+    <span className="tailwind">
+      <div className="opacity-0">temp</div>
+      <span className="relative">
+        一个链接
+        <span
+          className={classNames({
+            "absolute -top-8 left-1/2 transform -translate-x-1/2": true,
+            "px-2 py-1 text-xs rounded shadow-lg": true,
+            "pointer-events-none": true,
+            "whitespace-nowrap": true,
+            "text-white": true,
+            "bg-orange-500": true,
+          })}
+        >
+          站外链接
+          <span
+            className={classNames({
+              "absolute top-full left-1/2 transform -translate-x-1/2": true,
+              "border-l-4 border-r-4 border-t-4": true,
+              "w-0 h-0": true,
+              "border-transparent": true,
+              "border-t-orange-500": true,
+            })}
+          />
+        </span>
+      </span>
+      <span className="opacity-0">123</span>
+      <span className="relative">
+        两个链接
+        <span
+          className={classNames({
+            "absolute -top-8 left-1/2 transform -translate-x-1/2": true,
+            "px-2 py-1 text-xs rounded shadow-lg": true,
+            "pointer-events-none": true,
+            "whitespace-nowrap": true,
+            "text-white": true,
+            "bg-blue-500": true,
+          })}
+        >
+          站内文章
+          <span
+            className={classNames({
+              "absolute top-full left-1/2 transform -translate-x-1/2": true,
+              "border-l-4 border-r-4 border-t-4": true,
+              "w-0 h-0": true,
+              "border-transparent": true,
+              "border-t-blue-500": true,
+            })}
+          />
+        </span>
+      </span>
+      <span className="opacity-0">123</span>
+      <span className="relative">
+        三个链接
+        <span
+          className={classNames({
+            "absolute -top-8 left-1/2 transform -translate-x-1/2": true,
+            "px-2 py-1 text-xs rounded shadow-lg": true,
+            "pointer-events-none": true,
+            "whitespace-nowrap": true,
+            "text-white": true,
+            "bg-violet-500": true,
+          })}
+        >
+          页面内跳转
+          <span
+            className={classNames({
+              "absolute top-full left-1/2 transform -translate-x-1/2": true,
+              "border-l-4 border-r-4 border-t-4": true,
+              "w-0 h-0": true,
+              "border-transparent": true,
+              "border-t-violet-500": true,
+            })}
+          />
+        </span>
+      </span>
+      <div className="opacity-0">temp</div>
+    </span>
+  );
+};
+
+export default Link3;
