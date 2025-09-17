c50211935286ff30768fefe683ff0336d7cbbb3c
Casta-mere <castamerego@gmail.com>
2025-09-17 10:24:14 +0800

    system-modify: Add sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,6 +104,17 @@ const config = {
         blog: false,
         theme: {
           customCss: './src/css/custom.css',
+        }, sitemap: {
+          lastmod: 'date',
+          changefreq: 'monthly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+          createSitemapItems: async (params) => {
+            const {defaultCreateSitemapItems, ...rest} = params;
+            const items = await defaultCreateSitemapItems(rest);
+            return items.filter((item) => !item.url.includes('/page/'));
+          },
         },
       }),
     ],
