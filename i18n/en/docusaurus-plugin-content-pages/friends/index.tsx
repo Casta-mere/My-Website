2b74a068ded0d9c1b6c11c4e761857d6d4dd7ae8
Casta-mere <castamerego@gmail.com>
2025-10-09 11:09:41 +0800

    page-modify: friends page V1.1

--- i18n/en/docusaurus-plugin-content-pages/friends/index.tsx
+++ i18n/en/docusaurus-plugin-content-pages/friends/index.tsx
@@ -8,29 +8,26 @@ import { RoughNotation } from "react-rough-notation";
 export default function Friends() {
   return (
     <Layout>
-      <div className="row">
-        <div className="col col--9 col--offset-1">
-          <h1 className="hero__title  padding-top--lg">Friends 🔗</h1>
-          <p className="hero__subtitle">Friends are the Family we choose☀️</p>
-          <p className="">
-            <RoughNotation
-              type="underline"
-              show={true}
-              color="#32CD32"
-              strokeWidth={3}
-              animationDelay={2500}
-            >
-              Leave Ur site in the comment section below
-            </RoughNotation>
-          </p>
-          <button className="button button--secondary" onClick={JumpToComment}>
-            🙏 Add your site
-          </button>
-          <FriendCards />
+      <div className="container">
+        <h1 className="hero__title  padding-top--lg">Friends 🔗</h1>
+        <p className="hero__subtitle">Friends are the Family we choose☀️</p>
+        <p className="">
+          <RoughNotation
+            type="underline"
+            show={true}
+            color="#32CD32"
+            strokeWidth={3}
+            animationDelay={2500}
+          >
+            Leave Ur site in the comment section below
+          </RoughNotation>
+        </p>
+        <button className="button button--secondary" onClick={JumpToComment}>
+          🙏 Add your site
+        </button>
+        <FriendCards />
 
-          <div id="comment-anchor" />
-          <Comment />
-        </div>
+        <Comment />
       </div>
     </Layout>
   );
