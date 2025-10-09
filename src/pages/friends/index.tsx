ec2d6208be007fe2edc5db97e8276e44df3549b0
Casta-mere <castamerego@gmail.com>
2025-10-09 11:09:41 +0800

    page-modify: friends page V1.1

--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -8,28 +8,26 @@ import { RoughNotation } from "react-rough-notation";
 export default function Friends() {
   return (
     <Layout>
-      <div className="row">
-        <div className="col col--9 col--offset-1">
-          <h1 className="hero__title  padding-top--lg">友链 🔗</h1>
-          <p className="hero__subtitle">朋友是生活中的阳光☀️</p>
-          <p className="">
-            <RoughNotation
-              type="underline"
-              show={true}
-              color="#32CD32"
-              strokeWidth={3}
-              animationDelay={2500}
-            >
-              请通过评论留下你的网站
-            </RoughNotation>
-          </p>
-          <button className="button button--secondary" onClick={JumpToComment}>
-            🙏 添加你的网站
-          </button>
-          <FriendCards />
+      <div className="container">
+        <h1 className="hero__title padding-top--lg">友链 🔗</h1>
+        <p className="hero__subtitle">朋友是生活中的阳光☀️</p>
+        <p className="">
+          <RoughNotation
+            type="underline"
+            show={true}
+            color="#32CD32"
+            strokeWidth={3}
+            animationDelay={2500}
+          >
+            请通过评论留下你的网站
+          </RoughNotation>
+        </p>
+        <button className="button button--secondary" onClick={JumpToComment}>
+          🙏 添加你的网站
+        </button>
+        <FriendCards />
 
-          <Comment />
-        </div>
+        <Comment />
       </div>
     </Layout>
   );
